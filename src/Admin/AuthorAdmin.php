3c7508f1c5490f6d187ec7598da979d06e07e740
Nardberjean <Nardberjean@users.noreply.github.com>
2018-04-01 09:25:35 -0400

    Update AuthorAdmin.php

--- src/Admin/AuthorAdmin.php
+++ src/Admin/AuthorAdmin.php
@@ -6,7 +6,7 @@ use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
-class CategoryAdmin extends AbstractAdmin
+class AuthorAdmin extends AbstractAdmin
 {
     protected function configureFormFields(FormMapper $formMapper)
     {
